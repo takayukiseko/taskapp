bfb7e03e45c92e2282c464a58f058267249b3527
taka.seko <sekotaka.0613@gmail.com>
2020-03-01 09:05:32 +0900

    i created taskapp

---
+++ taskapp/Task.swift
@@ -0,0 +1,31 @@
+//
+//  Task.swift
+//  taskapp
+//
+//  Created by 瀬古貴之 on 2020/02/27.
+//  Copyright © 2020 takayuki.seko. All rights reserved.
+//
+
+import RealmSwift
+
+class Task: Object {
+    // 管理用 ID。プライマリーキー
+    @objc dynamic var id = 0
+
+    // タイトル
+    @objc dynamic var title = ""
+
+    // 内容
+    @objc dynamic var contents = ""
+
+    // 日時
+    @objc dynamic var date = Date()
+    
+    //カテゴリー
+    @objc dynamic var category = ""
+    
+    // id をプライマリーキーとして設定
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
